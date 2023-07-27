d4d02e213b1728bfb615e70bd15cd25551dc7bff
LeonardoRubuz <rubuzleonardo@gmail.com>
2023-07-27 02:57:38 +0100

    list group well rendered

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,6 +1,14 @@
 
 function ListGroup() {
-    return <h1>List Group</h1>
+    return (
+    <ul className="list-group">
+        <li className="list-group-item">Item 1</li>
+        <li className="list-group-item">Item 2</li>
+        <li className="list-group-item">Item 3</li>
+        <li className="list-group-item">Item 4</li>
+        <li className="list-group-item">Item 5</li>
+    </ul>
+    );
 }
 
 export default ListGroup
