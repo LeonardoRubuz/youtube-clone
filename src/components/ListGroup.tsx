39e8af25187e6a77e6a1b2449e40b0b696227113
LeonardoRubuz <rubuzleonardo@gmail.com>
2023-07-27 03:00:46 +0100

    a react comp only render one element and its children

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,15 @@
 
 function ListGroup() {
     return (
-    <ul className="list-group">
-        <li className="list-group-item">Item 1</li>
-        <li className="list-group-item">Item 2</li>
-        <li className="list-group-item">Item 3</li>
-        <li className="list-group-item">Item 4</li>
-        <li className="list-group-item">Item 5</li>
-    </ul>
+        <div>
+            <ul className="list-group">
+                <li className="list-group-item">Item 1</li>
+                <li className="list-group-item">Item 2</li>
+                <li className="list-group-item">Item 3</li>
+                <li className="list-group-item">Item 4</li>
+                <li className="list-group-item">Item 5</li>
+            </ul>
+        </div>
     );
 }
 
