d02a86a3499f5234be8f2dbf9ec2a73db449d428
LeonardoRubuz <rubuzleonardo@gmail.com>
2023-07-27 17:28:02 +0100

    displaying data

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,21 +1,22 @@
-//import { Fragment } from "react";
+import { Fragment } from "react";
 
 
 function ListGroup() {
-    const items = [
+    let items = [
         'New York',
         'San Francisco ' 
     ]
-    
+    //items = [];
+    // This is the default message to display whenever there is no data item to display
+    const message  = items.length === 0 && <p>No item found</p> 
     return (
         <>
             <h1>List</h1>
+            {message}
             <ul className="list-group">
-                <li className="list-group-item">Item 1</li>
-                <li className="list-group-item">Item 2</li>
-                <li className="list-group-item">Item 3</li>
-                <li className="list-group-item">Item 4</li>
-                <li className="list-group-item">Item 5</li>
+                { items.map((item) => (
+                    <li key={item}>{item}</li>
+                )) }
             </ul>
         </>
     );
